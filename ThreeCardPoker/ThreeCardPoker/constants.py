3d0cb7aa3be6b715d624211538c5102d87cb1da2
wgumenyuk <wlad@gumenyuk.de>
2022-12-13 21:05:47 +0100

    add constants

---
+++ ThreeCardPoker/ThreeCardPoker/constants.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+TITLE = "Three Card Poker"
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+FPS = 60
+TEXT_LIGHT_COLOR = (255, 255, 255) #FFFFFF
+BG_COLOR = (180, 120, 200) #B478C8
+
+class GameState(Enum):
+    """
+    Enum für alle möglichen Zustände des Spiels.
+    """
+
+    MENU = "MENU"
+    ANTE = "ANTE"
