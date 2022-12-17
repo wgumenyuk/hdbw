0f379fd442ea0f2b1a0245bbddc2e17992cb1889
wgumenyuk <wlad@gumenyuk.de>
2022-12-17 17:42:55 +0100

    add ante logic

--- ThreeCardPoker/ThreeCardPoker/states/ante.py
+++ ThreeCardPoker/ThreeCardPoker/states/ante.py
@@ -1,6 +1,9 @@
 import pygame as pg
 from states import BaseState
 from table import render_table
+from constants import TEXT_LIGHT_COLOR
+
+ANTE_STEP = 5
 
 class AnteState(BaseState):
     """
@@ -15,6 +18,21 @@ class AnteState(BaseState):
 
         super().__init__()
 
+        self.font = pg.font.Font(None, 24)
+        self.ante = 5
+        self.ante_text = self.font.render(f"Ante: ${self.ante}", True, TEXT_LIGHT_COLOR)
+    
+    def init(self, persistent_data: dict) -> None:
+        """
+        Initialisiert anhaltende Daten beim Eintreten eines neuen Zustandes.
+        """
+
+        super().init(persistent_data)
+        
+        self.balance = self.persistent_data["balance"] - self.ante
+        self.max_ante = self.balance / 2
+        self.balance_text = self.font.render(f"Guthaben: ${self.balance}", True, TEXT_LIGHT_COLOR)
+
     def handle_event(self, event: pg.event.Event) -> None:
         """
         Verarbeitet ein vom Spiel eingehendes Event.
@@ -27,7 +45,22 @@ class AnteState(BaseState):
             self.quit = True
             return
 
-        pass
+        if event.type == pg.KEYUP:
+            if event.key == pg.K_UP and self.balance - ANTE_STEP >= self.max_ante:
+                self.ante += ANTE_STEP
+                self.balance -= ANTE_STEP
+            elif event.key == pg.K_DOWN and self.ante - ANTE_STEP > 0:
+                self.ante -= ANTE_STEP
+                self.balance += ANTE_STEP
+            elif event.key == pg.K_RETURN:
+                self.next_state = "PAIR_PLUS" if self.ante < self.max_ante else "CARD_DRAWING"
+                self.persistent_data["balance"] = self.balance
+                self.is_done = True
+
+        self.balance_text = self.font.render(f"Guthaben: ${self.balance}", True, TEXT_LIGHT_COLOR)
+        self.ante_text = self.font.render(f"Ante: ${self.ante}", True, TEXT_LIGHT_COLOR)
 
     def render(self, screen: pg.Surface) -> None:
         render_table(screen)
+        screen.blit(self.balance_text, (50, 50))
+        screen.blit(self.ante_text, (50, 75))
