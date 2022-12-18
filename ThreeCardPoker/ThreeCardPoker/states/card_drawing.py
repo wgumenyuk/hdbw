9ee9175c0c1d1a77f2ef85e73d651f468d2e39cc
wgumenyuk <wlad@gumenyuk.de>
2022-12-18 16:23:31 +0100

    fix: docstring

--- ThreeCardPoker/ThreeCardPoker/states/card_drawing.py
+++ ThreeCardPoker/ThreeCardPoker/states/card_drawing.py
@@ -1,12 +1,11 @@
 import pygame as pg
 from states import BaseState
 from table import render_table
-from constants import TEXT_LIGHT_COLOR
 
 class CardDrawingState(BaseState):
     """
-    Paar-Plus-Zustand.
-    Das Spiel wartet auf eine optionale Pair-Plus-Wette des Spielers.
+    Card-Drawing-Zustand.
+    Es werden Karten für Spieler und Dealer gezogen.
     """
 
     def __init__(self) -> None:
@@ -39,13 +38,10 @@ class CardDrawingState(BaseState):
 
     def render(self, screen: pg.Surface) -> None:
         """
-        Rendert die Elemente im Paar-Plus-Zustand.
+        Rendert die Elemente im Card-Drawing-Zustand.
         
         Parameter:
         - `screen` (pg.Surface) - Bildschirm, auf den gerendert wird.
         """
 
         render_table(screen)
-        screen.blit(self.balance_text, (50, 50))
-        screen.blit(self.ante_text, (50, 75))
-        screen.blit(self.pair_plus_text, (50, 100))
