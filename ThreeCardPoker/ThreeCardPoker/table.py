679042276a5defdf475a5e33911418972a6c7a97
wgumenyuk <wlad@gumenyuk.de>
2022-12-18 16:40:14 +0100

    add: docstring

--- ThreeCardPoker/ThreeCardPoker/table.py
+++ ThreeCardPoker/ThreeCardPoker/table.py
@@ -7,4 +7,11 @@ table_path = path.join(base_path, "png/table.png")
 table = pg.image.load(table_path)
 
 def render_table(screen: pg.Surface) -> None:
+    """
+    Rendert den Tisch.
+
+    Parameter:
+    - `screen` (pg.Surface) - Fläche, auf der das Spiel gerendert wird.
+    """
+
     screen.blit(table, (0, 0))
