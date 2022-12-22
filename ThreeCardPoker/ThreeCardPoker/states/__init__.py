8d74a687279e58220441cd153727f3dd380edf96
wgumenyuk <wlad@gumenyuk.de>
2022-12-22 17:51:44 +0100

    add: play or fold state

--- ThreeCardPoker/ThreeCardPoker/states/__init__.py
+++ ThreeCardPoker/ThreeCardPoker/states/__init__.py
@@ -3,3 +3,4 @@ from states.menu import MenuState
 from states.ante import AnteState
 from states.pair_plus import PairPlusState
 from states.card_drawing import CardDrawingState
+from states.play_or_fold import PlayOrFoldState
