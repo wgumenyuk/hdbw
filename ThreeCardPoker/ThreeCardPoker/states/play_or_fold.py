7232c11e4c4e40b64a050ebe40589a760ce79d6d
wgumenyuk <wlad@gumenyuk.de>
2022-12-23 19:49:13 +0100

    refactor: play or fold

--- ThreeCardPoker/ThreeCardPoker/states/play_or_fold.py
+++ ThreeCardPoker/ThreeCardPoker/states/play_or_fold.py
@@ -1,9 +1,17 @@
 import pygame as pg
 from states import BaseState
-from card import Card
+from hand import Hand
 from table import render_table
 from chip import render_chip
-from constants import ANTE_CHIP_POS, PAIR_PLUS_CHIP_POS, HAND_PLAYER_CARD_POS, HAND_DEALER_CARD_POS, PLAY_CARD_POS
+from constants import (
+    TEXT_LIGHT_COLOR,
+    ANTE_CHIP_POS,
+    PAIR_PLUS_CHIP_POS,
+    HAND_PLAYER_CARD_POS,
+    HAND_DEALER_CARD_POS,
+    HAND_PLAYER_RANKING_TEXT_POS,
+    GameState
+)
 
 class PlayOrFoldState(BaseState):
     """
@@ -18,6 +26,8 @@ class PlayOrFoldState(BaseState):
 
         super().__init__()
 
+        self.font = pg.font.Font(None, 24)
+
     def init(self, persistent_data: dict) -> None:
         """
         Initialisiert anhaltende Daten beim Eintreten eines neuen Zustandes.
@@ -25,12 +35,15 @@ class PlayOrFoldState(BaseState):
 
         super().init(persistent_data)
 
+        self.balance: int = self.persistent_data["balance"]
         self.ante: int = self.persistent_data["ante"]
-        self.pair_plus: int = self.persistent_data["pair_plus"]
-        self.player_hand: list[Card] = persistent_data["player_hand"]
-        self.dealer_hand: list[Card] = persistent_data["dealer_hand"]
-
-        print("PLAY OR FOLD!")
+        self.pair_plus: int = self.persistent_data.get("pair_plus")
+        self.player_hand: Hand = self.persistent_data["player_hand"]
+        self.dealer_hand: Hand = self.persistent_data["dealer_hand"]
+        
+        self.balance_text = self.font.render(f"Guthaben: ${self.balance}", True, TEXT_LIGHT_COLOR)
+        self.hand_ranking_text = self.font.render(self.player_hand.get_ranking(), True, TEXT_LIGHT_COLOR)
+        self.hand_ranking_text_rect = self.hand_ranking_text.get_rect(center=HAND_PLAYER_RANKING_TEXT_POS)
 
     def handle_event(self, event: pg.event.Event) -> None:
         """
@@ -43,6 +56,20 @@ class PlayOrFoldState(BaseState):
         if event.type == pg.QUIT:
             self.quit = True
             return
+
+        if event.type == pg.KEYUP:
+            # Backspace
+            if event.key == pg.K_BACKSPACE:
+                self.next_state = GameState.FOLD
+                self.persistent_data["is_folded"] = True
+                self.is_done = True
+
+            # Enter
+            elif event.key == pg.K_RETURN:
+                self.next_state = GameState.PLAY
+                self.persistent_data["balance"] -= self.ante
+                self.persistent_data["is_folded"] = False
+                self.is_done = True
 
     def render(self, screen: pg.Surface) -> None:
         """
@@ -58,8 +85,8 @@ class PlayOrFoldState(BaseState):
         if self.pair_plus:
             render_chip(screen, self.pair_plus, PAIR_PLUS_CHIP_POS)
 
-        for i, card in enumerate(self.player_hand):
-            screen.blit(card.get_texture(), HAND_PLAYER_CARD_POS[i])
+        screen.blit(self.balance_text, (50, 50))
+        screen.blit(self.hand_ranking_text, self.hand_ranking_text_rect)
 
-        for i, card in enumerate(self.dealer_hand):
-            screen.blit(card.get_texture(hidden=True), HAND_DEALER_CARD_POS[i])
+        self.player_hand.render(screen, HAND_PLAYER_CARD_POS)
+        self.dealer_hand.render(screen, HAND_DEALER_CARD_POS, hidden=(True, True, True))
