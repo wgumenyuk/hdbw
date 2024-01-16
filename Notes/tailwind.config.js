5ed4c5bde1571d79fae338b61845b829f902ddf0
Wlad Gumenyuk <wlad@gumenyuk.de>
2024-01-16 20:11:24 +0100

    chore: add config files

---
+++ Notes/tailwind.config.js
@@ -0,0 +1,15 @@
+/**
+    @type {import("tailwindcss").Config}
+*/
+export default {
+    content: [
+        "./src/**/*.{html,js,svelte,ts}"
+    ],
+    theme: {
+        fontFamily: {
+            sans: "Inter, sans-serif"
+        },
+        extend: {}
+    },
+    plugins: []
+};
